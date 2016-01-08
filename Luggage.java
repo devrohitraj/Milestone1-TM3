c5c18fdc1019dc93ce5a4a9359086792bd5cbc5d
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-08 17:32:16 +0530

    Create Luggage.java

---
+++ Luggage.java
@@ -0,0 +1,10 @@
+package com.wipro.milestone1.TM3.Abstract.ques2;
+
+public class Luggage extends Compartment {
+
+	void notice() {
+		
+		System.out.println("Get your Luggage from here.");
+	}
+
+}
