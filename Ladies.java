bb779276f9cfd6e2c2e675543801725d556283f4
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-08 17:31:35 +0530

    Create Ladies.java

---
+++ Ladies.java
@@ -0,0 +1,11 @@
+package com.wipro.milestone1.TM3.Abstract.ques2;
+
+public class Ladies extends Compartment{
+
+	
+	void notice() {
+		
+		System.out.println("This class for Ladies only.");
+	}
+
+}
