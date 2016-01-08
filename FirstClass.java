c02b5c7457d31b6c989bd655c9175db20caee91d
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-08 17:31:15 +0530

    Create FirstClass.java

---
+++ FirstClass.java
@@ -0,0 +1,13 @@
+package com.wipro.milestone1.TM3.Abstract.ques2;
+
+public class FirstClass extends Compartment {
+
+	
+	void notice() {
+		
+		System.out.println("We are enjoying 1st Class.");
+	}
+	
+	
+
+}
