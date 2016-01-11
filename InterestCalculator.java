c4a8c06f6083d07f3c4c83017b5efa25900a4d41
Rohit Raj <devrohitraj@users.noreply.github.com>
2016-01-11 11:41:43 +0530

    Update InterestCalculator.java

--- InterestCalculator.java
+++ InterestCalculator.java
@@ -8,21 +8,31 @@ public class InterestCalculator {
 	
 	public static void main(String[] args) throws NumberFormatException, InputMismatchException {
 		
-	try{
-		FDAccount fda = new FDAccount();
-		SBAccount sba = new SBAccount();
-		RDAccount rda = new RDAccount();
-		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		//String choice1 = "y";
+       
+		while(flag){
+		//while(choice1.equalsIgnoreCase("y")){
 		
-		
-		System.out.println("Select the option :\n");
+        System.out.println("\nSelect the option :\n");
 		System.out.println("1. Interest Calculator-SB");
 		System.out.println("2. Interest Calculator-FB");
 		System.out.println("3. Interest Calculator-RB");
 		System.out.println("4. EXIT");
+        
+        
 		
-		int choice = sc.nextInt();
+	try{
+		FDAccount fda = new FDAccount();
+		SBAccount sba = new SBAccount();
+		RDAccount rda = new RDAccount();
 		
+		
+		int choice;
+		
+		Scanner sc = new Scanner(System.in);
+	    choice = sc.nextInt();
+	    
 		if(choice == 1){
 			sba.calculateInterest();
 		}
@@ -33,12 +43,13 @@ public class InterestCalculator {
 			rda.calculateInterest();
 		}
 		else if(choice == 4){
+			flag = false;
 			System.out.println("Thank you for using our Service. Exiting...");
 			System.exit(0);
 		}
 		else System.out.println("Please enter valid Option");
+	}
 
-	    }
 	    catch(InputMismatchException ime){
 	    	System.out.println("Please Enter Integer Only");
 	    }
@@ -50,6 +61,16 @@ public class InterestCalculator {
 			System.out.println("Some error occured, Please try again later.");
 			e.printStackTrace();
 		}
-    }
+	//System.out.println("\nPress y to continue and other to exit");
+    //Scanner sc = new Scanner(System.in);
+    
+    //choice1 = sc.nextLine();
+    //if(!choice1.equalsIgnoreCase("y")){
+    	//System.out.println("Thank you for using our service. System Exiting...");
+    	//System.exit(0);
+    //}
+	
+}
+	}
 
 }
